f1ef58185d190d08db59dc2a77a68d4191f15fca
Ivanramire5 <ivan.r4amire5@gmail.com>
2023-03-27 20:23:52 -0300

    maqueteando react

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,12 +1,15 @@
 import {Link} from "react-router-dom"
 
 
-const Item = ({id, name, price, category}) => {
+const Item = ({id, name, price, img, category}) => {
     return (
         <div className="ItemCajas">
-            <h2>{name}</h2>
-            <h3>${price}</h3>
-            <h4>{category}</h4>
+            <h2 className="ItemTitulo">{name}</h2>
+            <div className="ImagenItem">
+                <img src={img} alt={'Producto'}/>
+            </div>
+            <h3 className="ItemPrecio">${price}</h3>
+            <h4 className="ItemH4">{category}</h4>
             <Link to={`/item/${id}`}><button>Ver producto</button></Link>
         </div>
     )
