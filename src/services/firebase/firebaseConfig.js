96ed24dca7b75df3cef48bf4f8483df84031f358
Ivanramire5 <ivan.r4amire5@gmail.com>
2023-04-16 21:02:28 -0300

    proyecto React finalizado

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOFOyhyYsT4thz2X7aMskdrcBO9p5dUhY",
-  authDomain: "retro-store-83151.firebaseapp.com",
-  projectId: "retro-store-83151",
-  storageBucket: "retro-store-83151.appspot.com",
-  messagingSenderId: "1052314104667",
-  appId: "1:1052314104667:web:93ecc173a748a4d86b607e",
-  measurementId: "G-MNB3ZFXPDQ"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
 };
 
 
