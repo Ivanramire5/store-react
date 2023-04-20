3ac2632f5a58c505e38849d0b76fcb8a66c6e9d5
Ivanramire5 <ivan.r4amire5@gmail.com>
2023-04-20 03:23:15 -0300

    segundos cambios en responsabilidades. Falta retocar orders

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,85 +1,96 @@
-import React, { useContext, useState } from "react";
-import { CartContext } from "../../context/CartContext";
-import { Link } from 'react-router-dom';
+import { useContext } from 'react'
+import { CartContext } from '../../context/CartContext'
+import { Link } from 'react-router-dom'
+
 
 const Cart = () => {
+    const { cart } = useContext(CartContext)
 
-    const { cart } = useContext(CartContext);
-    const [nombre, setNombre] = useState('');
-    const [apellido, setApellido] = useState('');
-    const [telefono, setTelefono] = useState('');
-    const [codigoPostal, setCodigoPostal] = useState('');
-    const [direccion, setDireccion] = useState('');
-    const [ciudad, setCiudad] = useState('');
-    const [provincia, setProvincia] = useState('');
-    const [mail, setMail] = useState('')
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setNombre('');
-        setApellido('');
-        setTelefono('');
-        setCodigoPostal('');
-        setDireccion('');
-        setCiudad('');
-        setProvincia('');
-        setMail('');
-    }
-
-    return (
+    if (cart.length === 0 ){ 
+        return ('No hay productos')
+    }else {
+        return (
         <div>
-            <h1 className="carritoFinal">Cart</h1>
-            <div className="FormularioReact">
-                {
-                    cart.map(prod => {
-                        return (
-                            <div key={prod.id}>
-                                {prod.name}
-                            </div>
-                        )
-                    })
-                }
-                <div className="FormularioReact">
-                    <form className="EstilosFormulario" onSubmit={handleSubmit}>
-                        <label className="LabelFormulario">
-                            Nombre:
-                            <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
-                        </label>
-                        <label className="LabelFormulario">
-                            Apellido:
-                            <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
-                        </label>
-                        <label className="LabelFormulario">
-                            Teléfono:
-                            <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
-                        </label>
-                        <label className="LabelFormulario">
-                            Mail:
-                            <input type="text" value={mail} onChange={(e) => setMail(e.target.value)} />
-                        </label>
-                        <label className="LabelFormulario">
-                            Código Postal:
-                            <input type="text" value={codigoPostal} onChange={(e) => setCodigoPostal(e.target.value)} />
-                        </label>
-                        <label className="LabelFormulario">
-                            Dirección:
-                            <input type="text" value={direccion} onChange={(e) => setDireccion(e.target.value)} />
-                        </label>
-                        <label className="LabelFormulario">
-                            Ciudad:
-                            <input type="text" value={ciudad} onChange={(e) => setCiudad(e.target.value)} />
-                        </label>
-                        <label className="LabelFormulario">
-                            Provincia:
-                            <input type="text" value={provincia} onChange={(e) => setProvincia(e.target.value)} />
-                        </label>
-                    </form>
-                </div>
-                
+            <h1>Cart</h1>
+            <div>
+            {
+                cart.map(prod => {
+                    return (
+                        <div key={prod.id}>
+                            {prod.name}
+                            {prod.img}
+                            {prod.description}
+                        </div>
+                    
+                    )
+                })
+            }
             </div>
-            <Link className="botonCompra" to='/checkout'>Finalizar compra</Link>
+            <Link to='/checkout'>Finalizar compra</Link>
         </div>
-    )
+        )
+    }
 }
+export default Cart
 
-export default Cart;
+
+
+
+    // FORMULARIO !!!
+
+    // const { cart } = useContext(CartContext);
+    // const [nombre, setNombre] = useState('');
+    // const [apellido, setApellido] = useState('');
+    // const [telefono, setTelefono] = useState('');
+    // const [codigoPostal, setCodigoPostal] = useState('');
+    // const [direccion, setDireccion] = useState('');
+    // const [ciudad, setCiudad] = useState('');
+    // const [provincia, setProvincia] = useState('');
+    // const [mail, setMail] = useState('')
+    // const handleSubmit = (event) => 
+    //     event.preventDefault();
+    //     setNombre('');
+    //     setApellido('');
+    //     setTelefono('');
+    //     setCodigoPostal('');
+    //     setDireccion('');
+    //     setCiudad('');
+    //     setProvincia('');
+    //     setMail('');
+    // 
+    /* <div className="FormularioReact">
+    <form className="EstilosFormulario" onSubmit={handleSubmit}>
+    <label className="LabelFormulario">
+        Nombre:
+        <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+    </label>
+    <label className="LabelFormulario">
+        Apellido:
+        <input type="text" value={apellido} onChange={(e) => setApellido(e.target.value)} />
+    </label>
+    <label className="LabelFormulario">
+        Teléfono:
+        <input type="text" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+    </label>
+    <label className="LabelFormulario">
+        Mail:
+        <input type="text" value={mail} onChange={(e) => setMail(e.target.value)} />
+    </label>
+    <label className="LabelFormulario">
+        Código Postal:
+        <input type="text" value={codigoPostal} onChange={(e) => setCodigoPostal(e.target.value)} />
+    </label>
+    <label className="LabelFormulario">
+        Dirección:
+        <input type="text" value={direccion} onChange={(e) => setDireccion(e.target.value)} />
+    </label>
+    <label className="LabelFormulario">
+        Ciudad:
+        <input type="text" value={ciudad} onChange={(e) => setCiudad(e.target.value)} />
+    </label>
+    <label className="LabelFormulario">
+        Provincia:
+        <input type="text" value={provincia} onChange={(e) => setProvincia(e.target.value)} />
+    </label>
+    </form>
+    </div> */
--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,92 +1,315 @@
 import { useContext, useState } from 'react'
 import { CartContext } from '../../context/CartContext'
-// import { db } from '../../services/firebase/firebaseConfig'
-// import { collection, documentId, query, getDocs, where, writeBatch, addDoc } from "firebase/firestore"
-import { documentId, getDocs, query, collection, where, writeBatch, addDoc } from 'firebase/firestore' // Corregir la importación de doc
-// import { useNotification } from '../../notification/NotificationService'
+// import { db } from '../../service/firebase/firebaseConfig'
+// import { documentId, getDocs, query, collection, where, writeBatch, addDoc } from 'firebase/firestore'
+import { useNotification } from '../../notification/NotificationService'
 import { useNavigate } from 'react-router-dom'
-import '../../components/NavBar/NavBar.css'
+import { addOrder } from '../../service/firebase/firestore/orders'
+import { useAsync } from '../../hooks/useAsync'
 
-const Checkout = () => {
-// const [orderId, setOrderId] = useState('')
-// const [loading, setLoading] = useState(false)
-const { cart, total } = useContext(CartContext)
-const { orderId } = useParams ()
+function Form() {
+    const [user, setUser] = useState({
+        nombre: '',
+        apellido: '',
+        codigoPostal: '',
+        telefono: '',
+        correoElectronico: '',
+        direccion: ''
+    });
 
-const createOrderWithId = () => createOrder (orderId)
+    const handleChange = (event) => {
+        setUser({
+        ...user,
+        [event.target.name]: event.target.value
+        });
+    };
 
-const { data: orders, loading, error } = useAsync(createOrderWithId, [orderId])
-
-
-const handleConfirm = async (userData) => {
-    const objOrder {
-        buyer = {
-            buyer: {
-                name: '',
-                phone: '',
-                address: ''
-            },
-            items: cart,
-            total: total
-        }
-    }
-
-    // const ids = cart.map(prod => prod.id)
-
-    // const productRef = query(collection(db, 'products'), where(documentId(), 'in', ids))
-
-    // const productsAddedFromFirestore = await getDocs(productRef)
-
-    // const { docs } = productsAddedFromFirestore
-
-    // const batch = writeBatch(db)
-    // const outOfStock = []
-
-    // docs.forEach(doc => {
-    //     const dataDoc = doc.data()
-    //     const stockDb = dataDoc.stock
-
-        // const productoAgregadoAlCarrito = cart.find(prod => prod.id === doc.id)
-        // const prodCantidad = productoAgregadoAlCarrito?.cantidad
-
-    //     if(stockDb >= prodCantidad) {
-    //         batch.update(doc.ref, { stock: stockDb - prodCantidad})
-    //     } else {
-    //         outOfStock.push({ id: doc, ...dataDoc})
-    //     }
-    // })
-
-//     if(outOfStock.length === 0) {
-//         batch.commit()
-
-//         const orderRef = collection(db, 'orders')
-
-//         const orderAdded = await addDoc(orderRef, objOrder)
-//         clearCart()
-//         setOrderId(orderAdded.id)
-
-//     } else {
-//         setNotification('error', 'Hay productos que no tienen stock disponible')
-//     }
-//     } catch (error) {
-//         setNotification('error', 'Hubo un error generando la orden')
-//     } finally {
-//         setLoading(false)
-//     }
-// }
-
-    if(loading) {
-        return <h1>Se esta generando su pedido...</h1>
-    }
-
-return (
-    <div>
-        <h1>Checkout</h1>
+    const CampoDeFormulario = ({ label, name }) => {
+        return (
         <div>
-            <h2>El id de su pedido es: {orderId}</h2> : <button onClick={handleConfirm}>Generar orden</button> 
+            <label htmlFor={name}>{label}</label>
+            <input type="text" id={name} name={name} value={user[name]} onChange={handleChange} />
         </div>
-    </div>
-    )
+        );
+    };
+
+    return (
+        <form>
+        <CampoDeFormulario label="Nombre:" name="nombre" />
+        <CampoDeFormulario label="Apellido:" name="apellido" />
+        <CampoDeFormulario label="Código Postal:" name="codigoPostal" />
+        <CampoDeFormulario label="Teléfono:" name="telefono" />
+        <CampoDeFormulario label="Correo Electrónico:" name="correoElectronico" />
+        <CampoDeFormulario label="Dirección:" name="direccion" />
+        <button> terminar compra </button>
+        </form>
+    );
 }
 
-export default Checkout
+export default Form;
+
+// const Checkout = () => {
+//         const [orderId, setOrderId] = useState('') 
+//         const [loading, setLoading] = useState(false)
+//         const [user, setUser] = useState ({})
+
+//         const { cart, total, clearCart } = useContext(CartContext)
+//         const { setNotification } = useNotification()
+        
+//         const navigate = useNavigate()
+
+
+
+//         const updateUser = (event) => {
+//             setUser( user => ({...user, [event.target.name]: event.target.value }))
+//         }
+
+//             <form>
+//                 <label>
+//                     Nombre:
+//                     <input
+//                     type="text"
+//                     name="nombre"
+//                     value={user.nombre}
+//                     onChange={updateUser}
+//                 />
+//                 </label>
+//                 <label>
+//                     Apellido:
+//                     <input
+//                     type="text"
+//                     name="apellido"
+//                     value={user.apellido}
+//                     onChange={updateUser}
+//                 />
+//                 </label>
+//                 <label>
+//                     Código postal:
+//                     <input
+//                     type="text"
+//                     name="codigoPostal"
+//                     value={user.codigoPostal}
+//                     onChange={updateUser}
+//                 />
+//                 </label>
+//                 <label>
+//                 Teléfono:
+//                 <input
+//                     type="text"
+//                     name="telefono"
+//                     value={user.telefono}
+//                     onChange={updateUser}
+//                 />
+//                 </label>
+//                 <label>
+//                 Correo electrónico:
+//                 <input
+//                     type="email"
+//                     name="correoElectronico"
+//                     value={user.correoElectronico}
+//                     onChange={updateUser}
+//                 />
+//                 </label>
+//                 <label>
+//                 Dirección:
+//                 <input
+//                     type="text"
+//                     name="direccion"
+//                     value={user.direccion}
+//                     onChange={updateUser}
+//                 />
+//                 </label>
+//                 </form>
+//         // if(loading) {
+//         //     return <h1>Cargando...</h1>
+//         // }
+    
+//         // if(error) {
+//         //     return <h1>Vuelva a cargar la pagina</h1>
+//         // }
+    
+
+//         return <form setUser = {orders}/>
+// }
+// export default Checkout        
+
+    // const handleConfirm = async (userData) => {
+    //     try{ 
+    //         setLoading(true)
+    //         const objOrder = {
+    //             buyer: {
+    //                 name: '',
+    //                 phone: '',
+    //                 address: ''
+    //             },
+    //             items: cart,
+    //             total: total
+    //         }
+
+    //         const ids = cart.map(prod => prod.id)
+
+    //         const productRef = query(collection(db, 'products'), where(documentId(), 'in', ids))
+
+    //         const productsAddedFromFirestore = await getDocs(productRef)
+
+    //         const { docs } = productsAddedFromFirestore
+
+    //         const batch = writeBatch(db)
+    //         const outOfStock = []
+
+    //         docs.forEach(doc => {
+    //             const dataDoc = doc.data()
+    //             const stockDb = dataDoc.stock
+
+    //             const productAddedToCart = cart.find(prod => prod.id === doc.id)
+    //             const prodQuantity = productAddedToCart?.quantity
+
+    //             if(stockDb >= prodQuantity) {
+    //                 batch.update(doc.ref, { stock: stockDb - prodQuantity})
+    //             } else {
+    //                 outOfStock.push({ id: doc, ...dataDoc})
+    //             }
+    //         })
+
+    //         if(outOfStock.length === 0) {
+    //             batch.commit()
+
+    //             const orderRef = collection(db, 'orders')
+
+    //             const orderAdded = await addDoc(orderRef, objOrder)
+    //             clearCart()
+    //             setOrderId(orderAdded.id)
+
+    //             setTimeout(() => {
+    //                 navigate('/')
+    //             }, 5000)
+
+    //         } else {
+    //             setNotification('error', 'Hay productos que no tienen stock disponible')
+    //         } 
+    //     } catch (error) {
+    //         setNotification('error', 'Hubo un error generando la orden')
+    //     } finally {
+    //         setLoading(false)
+    //     }
+    // }
+    
+    // if(loading) {
+    //     return <h1>SE esta generando su orden...</h1>
+    // }
+
+    // return (
+    //     <div>
+    //         <h1>Checkout</h1>
+    //         { orderId ? <h2>El id de su orden es: {orderId}</h2> : <button onClick={handleConfirm}>Generar orden</button> }
+    //     </div>
+    // )
+
+    // // const [orderId, setOrderId] = useState ("")
+    // // const [loading, setLoading] = useState (false)
+    // // const {cart, total} = useContext (CartContext)
+    // const { orderId } = useParams ()
+
+    // const [user, setUser] = useState ({})
+
+    // const updateUser = (event) => {
+    //     setUser( user => ({...user, [event.target.name]: event.target.value }))
+    // }
+
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     setUser('');
+    //     setUser('');
+    //     setUser('');
+    //     setUser ('');
+    //     setUser('');
+    //     setUser('');
+    //     setUser('');
+    //     setUser('');
+    // }
+
+
+
+    // const createOrderWithId = () => createOrder (orderId)
+
+    // const { data: orders, loading, error } = useAsync(createOrderWithId, [orderId])
+
+    // // const handleConfirm = async (userData) => {
+    // //         const objOrder = {
+    // //             buyer: {
+    // //                 Name: "",
+    // //                 Phone: "",
+    // //                 Adress: ""
+    // //             }, 
+    // //             items: cart,
+    // //             total: total,
+    // //         }
+    // //         console.log (handleConfirm)
+    // //     }
+
+    // //         const ids = cart.map (prod => prod.id) 
+
+    // //         const productRef = query (collection (db, "products"), where (documentId(), 'in', ids))
+
+    // //         const productAddedFromFirestore = await getDocs(productRef)
+
+    // //         const { docs } = productAddedFromFirestore
+
+    // //         const batch = writeBatch (db)
+    // //         const outOfStock = []
+
+    // //         docs.forEach (doc =>{
+    // //             const dataDoc = doc.data ()
+    // //             const stockDb = dataDoc.stock
+
+    // //             const productAddedToCart = cart.find (prod => prod.id === doc.id)
+    // //             const prodQuantity = productAddedToCart?.quantity
+
+    // //             if (stockDb >= prodQuantity) {
+    // //                 batch.update (doc.ref, { stock: stockDb - prodQuantity})
+    // //             } else {
+    // //                 outOfStock.push ({ id: doc, ...dataDoc })
+    // //             }
+    // //         })
+
+    // //         if (outOfStock.length === 0) {
+    // //             batch.commit ()
+
+    // //             const orderRef = collection (db, "orders")
+
+    // //             const orderAdded = await addDoc (orderRef, objOrder)
+    // //             clearCart ()
+    // //             setOrderId (orderAdded.id)
+
+    // //             setTimeout (() => {
+    // //                 navigate ('/')
+    // //             }, 5000)
+
+    // //         } else {
+    // //             setNotification ("error", "Hay productos que no tienen stock disponible")
+    // //         }
+    // //     } catch (error) {
+    // //         setNotification ("error", "Hubo un error generando la orden")
+    // //     } finally {
+    // //         setLoading (false)
+    // //     }
+    // // return (
+    // //     // <div>
+    // //     //     <h1> Checkout </h1>
+    // // </div>
+    // // )
+
+    // if(loading) {
+    //     return <h1>Cargando...</h1>
+    // }
+
+    // if(error) {
+    //     return <h1>Vuelva a cargar la pagina</h1>
+    // }
+
+
+    // return (
+    //     <div>
+    //         {/* <h2>El id de su orden es: {orderId}</h2> : <button onClick={handleConfirm}>Generar orden</button> */}
+    //     </div>
+    // )
